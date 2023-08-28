82a03768feecf25b7c6e19f0f9ad3f1b7003114a
HargunS1 <136589718+HargunS1@users.noreply.github.com>
2023-08-28 22:42:10 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -4,35 +4,10 @@ provider "aws" {
   secret_key = "suOPrAo+y0bncrqnpCVRcgTZfMg50ydFsv/qzcrf"
 }
 
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
-}
 
 
 
 resource "aws_instance" "tfvm" {
   ami = "ami-01dd271720c1ba44f"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.websg.id ]
-  user_data = <<-EOF
-                #!/bin/bash
-                echo "I LOVE TERRAFORM" > index.html
-                nohup busybox httpd -f -p 8080 &
-                EOF
-    tags = {
-      Name = "WEB-demo"
-    }
-}
-resource "aws_security_group" "websg" {
-  name = "web-sg01"
-   vpc_id = aws_vpc.default.id
-  ingress {
-    protocol = "tcp"
-    from_port = 8080
-    to_port = 8080
-    cidr_blocks = [ "0.0.0.0/0" ]
   }
-}
-output "instance_ips" {
-  value = aws_instance.tfvm.public_ip
-}
